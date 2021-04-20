d70a97a0a8b10ff4520e81554fd2370ad12c0e01
Shamil Qurbonov <qurbonovshamil@gmail.com>
2021-04-20 20:18:32 +0700

    Fixed bugs

--- src/Doctrine/FlightSetIsFlightListener.php
+++ src/Doctrine/FlightSetIsFlightListener.php
@@ -2,20 +2,26 @@
 
 namespace App\Doctrine;
 
+use App\Constants\FlightConstants;
 use App\Entity\Flight;
 
 class FlightSetIsFlightListener
 {
     public function postUpdate(Flight $flight)
     {
-        if (!$flight->getIsFlightTicketSales()) {   // рейс отменён. TODO:: NEED TO SENDING EMAIL!
+        if ($flight->getStatus() === FlightConstants::CANCEL) {   // рейс отменён. TODO:: NEED TO SENDING EMAIL!
+            $emails = [];
+
+            foreach ($flight->getTickets() as $ticket) {
+                array_push($emails, $ticket->getPassengerEmail());
+            }
 //        $message = (new \Swift_Message('Рейс отменён'))
 //            ->setFrom('system@example.com')
 //            ->setTo('contact@les-tilleuls.coop')
 //            ->setBody(sprintf('Рейс #%d отменён.', $flight->getId()));
 //
 //        $this->mailer->send($message);
-            print_r('Flight canceled. Sending email!');
+            print_r($emails);
         }
     }
 }
